334d2f188e1a53dc6ee75b90ad9e25edc29f8158
Mac <taralgurung@hotmail.com>
2017-02-21 13:50:42 +0800

    My first Angular 2 app - task list- completed

--- app/app.component.js
+++ app/app.component.js
@@ -5,32 +5,35 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var core_1 = require("@angular/core");
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
         this.tasks = [
             new Task("Create To-Do List app.", 0),
             new Task("Learn Kung Fu.", 1),
             new Task("Rewatch all the Lord of the Rings movies.", 2),
-            new Task("Do the Laundry.", 3)
+            new Task("Do the laundry.", 3)
         ];
+        this.selectedTask = null;
     }
-    AppComponent.prototype.doStuff = function (clickedTask) {
-        if (clickedTask.done === true) {
-            alert("This Task is done!");
-        }
-        else {
-            alert("This Task is not completed yet! Get to work!");
-        }
+    AppComponent.prototype.showDetails = function (clickedTask) {
+        this.selectedTask = clickedTask;
     };
+    AppComponent.prototype.finishedEditing = function () {
+        this.selectedTask = null;
+    };
+    AppComponent = __decorate([
+        core_1.Component({
+            selector: 'my-app',
+            template: "\n  <div class=\"container\">\n    <h1>My First Angular 2 App</h1>\n    <div *ngFor=\"let currentTask of tasks\">\n      <h3>{{ currentTask.description }}</h3>\n      <button (click)=\"showDetails(currentTask)\">Edit</button>\n    </div>\n    <div *ngIf=\"selectedTask\">\n      <h1>Edit Task</h1>\n      <div>\n        <label>Enter Task Description:</label>\n        <input [(ngModel)]=\"selectedTask.description\">\n      </div>\n      <div>\n        <label>Enter Task ID:</label>\n        <input [(ngModel)]=\"selectedTask.id\">\n        <button (click)=\"finishedEditing()\">Done</button>\n      </div>\n    </div>\n  </div>\n  "
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
     return AppComponent;
 }());
-AppComponent = __decorate([
-    core_1.Component({
-        selector: 'my-app',
-        template: "\n  <div class=\"container\">\n   <h1>My First Angular 2 App</h1>\n   <h3 (click) = \"doStuff(currentTask)\" *ngFor=\"let currentTask of tasks\"> {{ currentTask.description}}</h3>\n  </div>\n  "
-    })
-], AppComponent);
 exports.AppComponent = AppComponent;
 var Task = (function () {
     function Task(description, id) {
--- app/app.component.ts
+++ app/app.component.ts
@@ -4,29 +4,44 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
   <div class="container">
-   <h1>My First Angular 2 App</h1>
-   <h3 (click) = "doStuff(currentTask)" *ngFor="let currentTask of tasks"> {{ currentTask.description}}</h3>
+    <h1>My First Angular 2 App</h1>
+    <div *ngFor="let currentTask of tasks">
+      <h3>{{ currentTask.description }}</h3>
+      <button (click)="showDetails(currentTask)">Edit</button>
+    </div>
+    <div *ngIf="selectedTask">
+      <h1>Edit Task</h1>
+      <div>
+        <label>Enter Task Description:</label>
+        <input [(ngModel)]="selectedTask.description">
+      </div>
+      <div>
+        <label>Enter Task ID:</label>
+        <input [(ngModel)]="selectedTask.id">
+        <button (click)="finishedEditing()">Done</button>
+      </div>
+    </div>
   </div>
   `
 })
 
 export class AppComponent {
   public tasks: Task[] = [
-    new Task("Create To-Do List app.", 0),
-    new Task("Learn Kung Fu.",1),
-    new Task("Rewatch all the Lord of the Rings movies.",2),
-    new Task("Do the Laundry.",3)
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu.", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
   ];
-  doStuff(clickedTask: Task) {
-    if (clickedTask.done === true) {
-      alert("This Task is done!");
-    } else {
-      alert("This Task is not completed yet! Get to work!");
-    }
+  selectedTask: Task = null;
+  showDetails(clickedTask: Task) {
+    this.selectedTask = clickedTask;
+  }
+  finishedEditing() {
+    this.selectedTask = null;
   }
 }
 
 export class Task {
-  public done: boolean=false;
-  constructor(public description: string, public id: number) {}
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {   }
 }
